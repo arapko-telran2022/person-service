cc2276105bdb17bc57ee6e95e854468c551dd53c
arapko <1990rapko@gmail.com>
2022-11-28 14:45:57 +0200

    Join relation commit

--- src/main/java/telran/java2022/person/service/PersonServiceImpl.java
+++ src/main/java/telran/java2022/person/service/PersonServiceImpl.java
@@ -1,24 +1,31 @@
 package telran.java2022.person.service;
 
 import java.time.LocalDate;
+import java.util.Map;
 import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
 
 import org.modelmapper.ModelMapper;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import lombok.RequiredArgsConstructor;
 import telran.java2022.person.dao.PersonRepository;
 import telran.java2022.person.dto.AddressDto;
+import telran.java2022.person.dto.ChildDto;
 import telran.java2022.person.dto.CityPopulationDto;
+import telran.java2022.person.dto.EmployeeDto;
 import telran.java2022.person.dto.PersonDto;
 import telran.java2022.person.dto.exceptions.PersonNotFoundException;
 import telran.java2022.person.model.Address;
+import telran.java2022.person.model.Child;
+import telran.java2022.person.model.Employee;
 import telran.java2022.person.model.Person;
 
 @Service
 @RequiredArgsConstructor
-public class PersonServiceImpl implements PersonService {
+public class PersonServiceImpl implements PersonService, CommandLineRunner  {
 	
 	final PersonRepository personRepository;
 	final ModelMapper modelMapper;
@@ -93,5 +100,39 @@ public class PersonServiceImpl implements PersonService {
 	public Iterable<CityPopulationDto> getCitiesPopulation() {
 		return personRepository.getCitiesPopulation();
 	}
+	
+	@Override
+	public void run(String... args) throws Exception {
+		Person person = new Person(1000, "John", LocalDate.of(1985, 4, 11), new Address("Tel Aviv", "Ben Gvirol", 87));
+		Child child = new Child(2000, "Mosche", LocalDate.of(2018, 7, 5), new Address("Ashkelon", "Bar Kohva", 21), "Shalom");
+		Employee employee = new Employee(3000, "Sarah", LocalDate.of(1995, 11, 23), new Address("Rehovot", "Herzl", 7), "Motorola", 20000);
+		personRepository.save(person);
+		personRepository.save(child);
+		personRepository.save(employee);
+		
+	}
+	
+	private Class<? extends Person> getModelClass(PersonDto personDto) {
+		if (personDto instanceof EmployeeDto) {
+			return Employee.class;
+		}
+		if (personDto instanceof ChildDto) {
+			return Child.class;
+		}
+		return Person.class;
+	}
+
+	@Override
+	public Iterable<PersonDto> findEmployeeBySalary(int min, int max) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Iterable<PersonDto> getChildren() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
 
 }
